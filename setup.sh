1640438279c9aa44019dc8b4376afaa928bf7944
Alex Svatukhin <alex.svatukhin@mongodb.com>
2025-07-30 11:06:26 -0700

    changes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-
-
-
-
 if [[ "$(uname)" = "Linux" ]]; then
     echo "its linux"
     mkdir -p ~/.config
@@ -22,10 +17,10 @@ elif [[ "$(uname)" = "Darwin" ]]; then
 
     echo "its macos"
 
-    ln -s ~/dotfiles/config/kitty ~/.config/kitty
     ln -s ~/dotfiles/config/nvim ~/.config/nvim
     ln -s ~/dotfiles/config/tmux.conf ~/.tmux.conf
     ln -s ~/dotfiles/config/zshrc ~/.zshrc
-    # TODO: iterm2
+    # TODO: make sure to point iterm2 to ~/dotfiles/config/iterm2
+    echo "make sure to point iterm2 to ~/dotfiles/config/iterm2"
 fi
 
