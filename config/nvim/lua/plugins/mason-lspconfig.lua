7eb2c1017b66952cd88ec872a444a61f4ae54008
Alex Svatukhin <alex.svatukhin@mongodb.com>
2025-08-07 18:53:53 -0700

    changes

--- config/nvim/lua/plugins/mason-lspconfig.lua
+++ config/nvim/lua/plugins/mason-lspconfig.lua
@@ -3,7 +3,8 @@ return({
     opts = {
         ensure_installed = { 
             "lua_ls", 
-            "gopls"
+            "gopls",
+            "jsonls"
         },
         automatic_enable = true
     },
